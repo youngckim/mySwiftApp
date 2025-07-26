bc9e42922491345e43cce9dbb338eb2a8236aeee
young kim <youngckim@yahoo.com>
2025-07-26 18:33:25 -0400

    Initial Commit

---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by young kim on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
